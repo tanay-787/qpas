1e0e285d62812a0acc9d2a1b5bd6a952cef0c2a8
Tanay <nevertrustesports@gmail.com>
2025-04-01 07:31:23 +0530

    "assert" patch

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,6 +1,6 @@
 import admin from "firebase-admin";
 
-import serviceAccount from "./serviceAccountKey.json" as { type: "json" };
+import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
