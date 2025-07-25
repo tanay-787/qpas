54074f369f1d0cb48d3b1f110c918b77e028bbc5
tanayyyyy7 <tanayyyyy7@gmail.com>
2025-07-25 13:12:16 +0000

    tweaked setup

--- backend/server.js
+++ backend/server.js
@@ -62,3 +62,5 @@ app.get("/health", (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
+
+export default app;
